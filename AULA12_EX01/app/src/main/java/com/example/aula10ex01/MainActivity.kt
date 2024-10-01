4db008d3e140fa0b61ef08d6cc76a36363c01f40
Prof. André Luiz Braga, D.Sc <andre.luiz.braga@terra.com.br>
2024-10-01 10:01:38 -0300

    Aula 12 Activity Ida e Volta

---
+++ AULA12_EX01/app/src/main/java/com/example/aula10ex01/MainActivity.kt
@@ -0,0 +1,201 @@
+package com.example.aula10ex01
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment.SavedState
+import java.io.Serializable
+
+class Peca : Serializable {
+    private var nomePeca: String
+    private var codigoPeca: String
+    private var valorPeca: Float
+    private var custoPeca: Float
+    private var servicoPeca: Float
+
+    constructor(
+        nomePeca: String,
+        codigoPeca: String,
+        valorPeca: Float,
+        custoPeca: Float,
+        servicoPeca: Float
+    ) {
+        this.nomePeca = nomePeca
+        this.codigoPeca = codigoPeca
+        this.valorPeca = valorPeca
+        this.custoPeca = custoPeca
+        this.servicoPeca = servicoPeca
+    }
+
+    fun total(): Float {
+        return (valorPeca + servicoPeca)
+    }
+
+    fun lucro(): Float {
+        return (valorPeca - custoPeca + servicoPeca)
+    }
+
+    override fun toString(): String {
+        return "Peca('$nomePeca' - '$codigoPeca' = R$ $valorPeca)"
+    }
+
+    fun getNomePeca(): String {
+        return nomePeca
+    }
+
+    fun getCodigoPeca(): String {
+        return codigoPeca
+    }
+
+    fun getValorPeca(): Float {
+        return valorPeca
+    }
+
+    fun getCustoPeca(): Float {
+        return custoPeca
+    }
+
+    fun getServicoPeca(): Float {
+        return servicoPeca
+    }
+
+}
+
+
+
+
+class MainActivity : AppCompatActivity() {
+    private var nomePeca:EditText? = null
+    private var codigoPeca:EditText? = null
+    private var valorPeca:EditText? = null
+    private var custoPeca:EditText? = null
+    private var servicoPeca:EditText? = null
+    private var botaoInsere:Button? = null
+    private var totalPecas:TextView? = null
+    private var lucroPecas:TextView? = null
+    private var listaPecas: ArrayList<Peca>? = null
+    private var listaRecuperada:ArrayList<Peca>? = null
+
+    private var adaptador:ArrayAdapter<Peca>? = null
+    private var adaptadorSP:ArrayAdapter<Peca>? = null
+    private var listaPecasView:ListView? = null
+    private var listaSpinner: Spinner?= null
+
+    fun inicializacao(){
+        nomePeca = findViewById(R.id.ID1_NOMEPECAeditTextText)
+        codigoPeca = findViewById(R.id.ID1_CODIGOeditTextText2)
+        valorPeca = findViewById(R.id.ID1_VALOReditTextNumber2)
+        custoPeca = findViewById(R.id.ID1_CUSTOeditTextNumber)
+        servicoPeca = findViewById(R.id.ID1_SERVICOeditTextNumber3)
+        botaoInsere = findViewById(R.id.ID1_INSEREbutton)
+        totalPecas = findViewById(R.id.ID1_TOTALtextView7)
+        lucroPecas = findViewById(R.id.ID1_LUCROtextView6)
+        listaPecasView = findViewById(R.id.ID1_LISTA_ListView)
+        listaSpinner   = findViewById(R.id.ID1_LISTAspinner)
+
+        listaPecas = ArrayList<Peca>()
+
+        adaptador = ArrayAdapter<Peca>(this,android.R.layout.simple_list_item_1, listaPecas!!)
+        listaPecasView?.adapter = adaptador
+        adaptadorSP = ArrayAdapter<Peca>(this,android.R.layout.simple_spinner_item, listaPecas!!)
+        adaptadorSP?.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        listaSpinner?.adapter = adaptadorSP
+
+        botaoInsere?.setOnClickListener {
+            var nome   = nomePeca?.text.toString()
+            var codigo = codigoPeca?.text.toString()
+            var valor  = valorPeca?.text.toString().toFloat()
+            var custo  = custoPeca?.text.toString().toFloat()
+            var servico = servicoPeca?.text.toString().toFloat()
+            var pecaInserida = Peca(nome,codigo,valor,custo,servico)
+            listaPecas?.add(pecaInserida)
+            adaptador?.notifyDataSetChanged()
+            adaptadorSP?.notifyDataSetChanged()
+            var total:Float = 0F
+            var lucro:Float = 0F
+            for(peca in listaPecas!!){
+                total += peca.total()
+                lucro += peca.lucro()
+            }
+            totalPecas?.text = "R$ ${total}"
+            lucroPecas?.text = "R$ ${lucro}"
+        }
+        listaPecasView?.setOnItemClickListener { adaptador, view, i, l ->
+            nomePeca?.setText(listaPecas?.get(i)?.getNomePeca() )
+            codigoPeca?.setText(listaPecas?.get(i)?.getCodigoPeca())
+            valorPeca?.setText("${listaPecas?.get(i)?.getValorPeca()}")
+            custoPeca?.setText("${listaPecas?.get(i)?.getCustoPeca()}")
+            servicoPeca?.setText("${listaPecas?.get(i)?.getServicoPeca()}")
+
+            val pacoteDetalhe = Bundle()
+            pacoteDetalhe.putString("NOME",nomePeca?.text.toString())
+            pacoteDetalhe.putString("CODIGO",codigoPeca?.text.toString())
+            pacoteDetalhe.putString("PRECO",valorPeca?.text.toString())
+            pacoteDetalhe.putString("CUSTO",custoPeca?.text.toString())
+            pacoteDetalhe.putString("SERVICO",servicoPeca?.text.toString())
+            pacoteDetalhe.putSerializable("LISTA",listaPecas)
+
+            val detalheIntent = Intent(this,PecasActivity::class.java)
+
+            detalheIntent.putExtras(pacoteDetalhe)
+            startActivity(detalheIntent)
+        }
+        /*
+        listaSpinner?.setOnItemClickListener { adaptadorSP, view, i, l ->
+            nomePeca?.setText(listaPecas?.get(i)?.getNomePeca() )
+            codigoPeca?.setText(listaPecas?.get(i)?.getCodigoPeca())
+            valorPeca?.setText("${listaPecas?.get(i)?.getValorPeca()}")
+            custoPeca?.setText("${listaPecas?.get(i)?.getCustoPeca()}")
+            servicoPeca?.setText("${listaPecas?.get(i)?.getServicoPeca()}")
+
+            val pacoteDetalhe = Bundle()
+            pacoteDetalhe.putString("NOME",nomePeca?.text.toString())
+            pacoteDetalhe.putString("CODIGO",codigoPeca?.text.toString())
+            pacoteDetalhe.putString("PRECO",valorPeca?.text.toString())
+            pacoteDetalhe.putString("CUSTO",custoPeca?.text.toString())
+            pacoteDetalhe.putString("SERVICO",servicoPeca?.text.toString())
+
+            val detalheIntent = Intent(this,PecasActivity::class.java)
+            detalheIntent.putExtras(pacoteDetalhe)
+            startActivity(detalheIntent)
+        }
+        */
+
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        inicializacao()
+        var pacoteChecada: Bundle? = intent.extras
+        if (intent.extras !=null){
+            listaPecas = pacoteChecada?.getSerializable("LISTA") as ArrayList<Peca>
+            adaptador = ArrayAdapter<Peca>(this,android.R.layout.simple_list_item_1, listaPecas!!)
+            listaPecasView?.adapter = adaptador
+        }
+    }
+
+    ///////////////////////////////////////////////
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        print("Salvando")
+        outState.putSerializable("LISTA",listaPecas)
+    }
+}
